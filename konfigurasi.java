244402ed7bdd0cc2503f1d41f77cf14f92a7f1a6
Hadriansa <ansar080489@gmail.com>
2022-07-05 08:21:08 +0800

    Add files via upload

---
+++ konfigurasi.java
@@ -0,0 +1,30 @@
+package com.example.siakad;
+
+public class konfigurasi {
+    public static final String URL_GET_ALL="http://http://127.0.0.1/siakad/getdata.php";
+    public static final String URL_ADD="http://http://127.0.0.1//siakad/tambah.php";
+    public static final String URL_GET_EMP="http://http://127.0.0.1//siakad/tampildata.php?id=";
+
+    //JSON Tags
+    public static final     String TAG_JSON_ARRAY="mahasiswa";
+    public static final String TAG_NIM="nim";
+    public static final String TAG_NAMA="nama";
+    public static final String TAG_JURUSAN="jurusan";
+    public static final String TAG_TEMPATLAHIR="tmplahir";
+    public static final String TAG_TANGGALLAHIR="tgllahir";
+    public static final String TAG_ALAMAT="alamat";
+    public static final String TAG_AGAMA="agama";
+
+
+    //kunci yang digunakan untuk mengirim data
+    public static final String KEY_EMP_NIM="nim";
+    public static final String KEY_EMP_NAMA="nama";
+    public static final String KEY_EMP_JURUSAN="jurusan";
+    public static final String KEY_EMP_TEMPATLAHIR="tmplahir";
+    public static final String KEY_EMP_TANGGALLAHIR="tgllahir";
+    public static final String KEY_EMP_ALAMAT="alamat";
+    public static final String KEY_EMP_AGAMA="agama";
+
+
+    public static  final String EMP_ID="emp_id";
+}
